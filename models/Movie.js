a7a328b6acc448da23ecbf5dd334409b0229692c
baltasar torres garcia <baltasartorresgarcia@MacBook-Pro-de-amelia.local>
2019-03-11 16:29:36 +0100

    no vale

--- models/Movie.js
+++ models/Movie.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
   tittle: String,
   genero: String
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
 
 const Movie = mongoose.model('Movie', movieSchema);
-module.exports = User;
+module.exports = Movie;
--- routes/movie.js
+++ routes/movie.js
@@ -1,4 +1,4 @@
 const Router = require("express").Router();
 const Movie = require("../models/Movie");
 
-module.exports = router;
+module.exports = Router;
